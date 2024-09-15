ce0d1f57ca0820dccb7938a4fa720c082e02e25b
Alan Bui <alanb3350@gmail.com>
2024-09-15 04:08:54 -0400

    changed file locations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 export default function Navbar() {
   return (
     <><div className = "navbar">
-      <img className="logo-icon"src = "./src/assets/logo_icon.png"></img>
-      <img className="logo-loo"src = "./src/assets/logo_loo.png"></img>
+      <img className="logo-icon"src = "https://loolooloo.vercel.app/logo_icon.png"></img>
+      <img className="logo-loo"src = "https://loolooloo.vercel.app/logo_loo.png"></img>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <div className="home-div">
-        <img className="logo-icon-home" src="./src/assets/logo_icon.png"></img>
-        <img className="logo-loo-home" src="./src/assets/logo_loo.png"></img>
+        <img className="logo-icon-home" src="https://loolooloo.vercel.app/assets/logo_icon.png"></img>
+        <img className="logo-loo-home" src="https://loolooloo.vercel.app/assets/logo_loo.png"></img>
         <NavLink to = "/login"><button className="login-page-button" >TO DA LOO</button></NavLink>
         <button className="create-account-button">CREATE AN ACCOUNT</button>
       </div>
