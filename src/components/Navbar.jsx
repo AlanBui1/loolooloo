b7f15c85b436533b255c5736cbd83a82e5c76c28
Alan Bui <alanb3350@gmail.com>
2024-09-15 04:25:59 -0400

    added image links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 export default function Navbar() {
   return (
     <><div className = "navbar">
-      <img className="logo-icon"src = "https://loolooloo.vercel.app/logo_icon.png"></img>
-      <img className="logo-loo"src = "https://loolooloo.vercel.app/logo_loo.png"></img>
+      <img className="logo-icon"src = "https://www.alanbui.ca/assets/images/hackthenorth/logo_icon.png"></img>
+      <img className="logo-loo"src = "https://www.alanbui.ca/assets/images/hackthenorth/logo_loo.png"></img>
       </div>
     </>
   );
