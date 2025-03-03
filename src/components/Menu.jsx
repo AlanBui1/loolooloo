ca11b5270e6549228204d611ed410848a1ec75e9
Alan Bui <alanb3350@gmail.com>
2025-03-03 16:56:01 -0500

    changed links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 export default function Menu() {
   return (
     <><div className = "menu">
-      <NavLink to = "/map"><img className="map-icon"src = "/assets/images/hackthenorth/map-icon.png"></img></NavLink>
-      <NavLink to = "/waterlog"><img className="waterlog-icon"src = "/assets/images/hackthenorth/waterlog-icon.png"></img></NavLink>
+      <NavLink to = "/map"><img className="map-icon"src = "/assets/map-icon.png"></img></NavLink>
+      <NavLink to = "/waterlog"><img className="waterlog-icon"src = "/assets/waterlog-icon.png"></img></NavLink>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import '../App.css'
 
 export default function Navbar() {
   return (
-    <><div className = "navbar">
-      <img className="logo-icon"src = "/assets/images/hackthenorth/logo_icon.png"></img>
-      <img className="logo-loo"src = "/assets/images/hackthenorth/logo_loo.png"></img>
+    <div className = "navbar">
+      <img className="logo-icon"src = "/assets/logo_icon.png"></img>
+      <img className="logo-loo"src = "/assets/logo_loo.png"></img>
       </div>
   );
 }
